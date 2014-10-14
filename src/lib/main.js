2ccb37a2bd208d0c6b1bd830bc23ab4fb5eba51d
KS Chan <mrkschan@gmail.com>
2014-10-14 09:00:49 +0800

    Started ActionButton

---
+++ src/lib/main.js
@@ -0,0 +1,12 @@
+var buttons = require('sdk/ui/button/action');
+
+var isgdButton = buttons.ActionButton({
+  id: "isgdcreator-button",
+  label: "Create is.gd shortened URL for the current page",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png"
+  },
+  onClick: function(state) {
+  }
+});
