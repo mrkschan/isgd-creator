a88a1291c2fd068e834dcac7835ba14d16237e12
KS Chan <mrkschan@gmail.com>
2014-11-09 19:06:44 +0800

    Extracted the creation of isgd URL as a helper function

--- src/lib/main.js
+++ src/lib/main.js
@@ -6,6 +6,7 @@ var buttons = require('sdk/ui/button/action'),
     tabs = require('sdk/tabs'),
     urls = require('sdk/url');
 
+// Setup action button
 var isgdButton = buttons.ActionButton({
   id: 'isgdcreator-button',
   label: 'Create is.gd shortened URL for the current page',
@@ -14,24 +15,30 @@ var isgdButton = buttons.ActionButton({
     '32': './icon-32.png'
   },
   onClick: function(state) {
-    var url = tabs.activeTab.url;
-
-    if (!urls.isValidURI(url)) {
-      return;
-    }
-
-    requests.Request({
-      url: 'http://is.gd/api.php?longurl=' + escape(url),
-      onComplete: function(response) {
-        var isgd = response.text;
-
-        clipboard.set(isgd, 'text');
-        notifications.notify({
-          title: 'is.gd Creator',
-          text: 'URL copied to clipboard - ' + isgd,
-          iconURL: self.data.url('icon-32.png')
-        });
-      }
-    }).get();
+    isgdify(tabs.activeTab.url);
   }
 });
+
+  }
+});
+
+// Helpers
+function isgdify(url) {
+  if (!urls.isValidURI(url)) {
+    return;
+  }
+
+  requests.Request({
+    url: 'http://is.gd/api.php?longurl=' + escape(url),
+    onComplete: function(response) {
+      var isgd = response.text;
+
+      clipboard.set(isgd, 'text');
+      notifications.notify({
+        title: 'is.gd Creator',
+        text: 'URL copied to clipboard - ' + isgd,
+        iconURL: self.data.url('icon-32.png')
+      });
+    }
+  }).get();
+}
